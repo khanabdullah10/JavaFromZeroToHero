5106492250f6a32fa2060707ccd19483052b1797
khan Abdullah <khanabdulla7862@gmail.com>
2025-09-30 19:21:11 +0530

    30-09-25

---
+++ Patterns/src/com/practice/striverAtoZ/Patterns.java
@@ -0,0 +1,131 @@
+package com.practice.striverAtoZ;
+
+public class Patterns {
+
+
+
+    public static void main(String[] args) {
+        /**
+         Pattern 1
+         *
+         **
+         ***
+         ****
+         *****
+         */
+        System.out.println("Pattern 1");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <=5 ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+        /**
+         Pattern 2
+         *
+         **
+         ***
+         ****
+         *****
+         */
+        System.out.println("Pattern 2");
+        for (int i = 1; i <=4 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        /**
+         Pattern 3
+         1
+         12
+         123
+         1234
+         12345
+         */
+        System.out.println("Pattern 3");
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        /**
+         Pattern 4
+         1
+         22
+         333
+         4444
+         55555
+         */
+        System.out.println("Pattern 4");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        /**
+         Pattern 5
+         *****
+         ****
+         ***
+         **
+         *
+         */
+        System.out.println("Pattern 5");
+
+        for (int i = 5; i >=1 ; i--) {
+            for (int j = i; j >=1 ; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        /**
+         Pattern 6
+         12345
+         1234
+         123
+         12
+         1
+         */
+        System.out.println("Pattern 6");
+
+        for (int i = 5; i >=1 ; i--) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+
+        /**
+         Pattern 7
+                    *
+                  * * *
+                 * * * *
+                * * * * * *
+               * * * * * * *
+         */
+        System.out.println("Pattern 7");
+
+        for (int i = 5; i >=1 ; i--) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+                for (int k = 0; k < ; k++) {
+                    
+                }
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+}
